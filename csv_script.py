ae1ee6e1adac6abf41da182dc5cbf39f71e8b27a
Quenom117 <dmonteiro117@gmail.com>
2025-05-16 12:18:20 -0300

    erro no safe read

--- csv_script.py
+++ csv_script.py
@@ -9,10 +9,10 @@ import numpy as np
 from tqdm import tqdm
 
 # Importa os métodos de descritores
-#from methods.m01_spinimages import extract_spinimages
-#from methods.m02_PFH import extract_pfh
-#from methods.m03_FPFH import extract_fpfh
-#from methods.m04_SHOT import extract_shot
+# from methods.m01_spinimages import extract_spinimages
+# from methods.m02_PFH import extract_pfh
+# from methods.m03_FPFH import extract_fpfh
+# from methods.m04_SHOT import extract_shot
 from methods.C14_RGB import compute_rgb_covariance_descriptor
 
 # =======================
@@ -20,51 +20,62 @@ from methods.C14_RGB import compute_rgb_covariance_descriptor
 # =======================
 INPUT_DATASET_CSV = "/home/dani/Estudos/PIBIC/APSIPA___M-PCCD/apsipa.csv"
 OUTPUT_FEATURES_CSV = "/home/dani/Estudos/PIBIC/APSIPA___M-PCCD/saida_features.csv"
-FEATURES_EXTRACTOR = "rgb_covariance"  # Altere para outros conforme necessário
+FEATURES_EXTRACTOR = "rgb_covariance" # Altere para outros conforme necessário
+
 
 # =======================
 # FUNÇÕES AUXILIARES
 # =======================
 
-# Função de leitura de nuvem de pontos
 def safe_read_point_cloud(path: str):
-    try:
-        pc = o3d.io.read_point_cloud(path)
-        return pc
-    except Exception as e:
-        print(f"Erro ao ler {path}: {e}")
-        return None #### ATENCAO
+    
+    
+    """
+    Lê nuvem de pontos de um arquivo .ply usando Open3D. 
+    Se falhar, converte o arquivo para um formato compatível via pymeshlab e tenta novamente.
+    """
+    def get_suitable_version_path(ply_path, temp_dir):
+        os.makedirs(temp_dir, exist_ok=True)
+        temp_ply_filepath = os.path.join(temp_dir, "temp.ply")
+        ms = pymeshlab.MeshSet()
+        ms.load_new_mesh(ply_path)
+        ms.save_current_mesh(temp_ply_filepath)
+        return temp_ply_filepath
 
-# Mapeamento dos descritores para suas funções
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with io.StringIO() as buf, redirect_stdout(buf):
+            pc = o3d.io.read_point_cloud(path)
+            output = buf.getvalue()
+        if "Read PLY failed" in output or len (np.array(pc.points)) == 0:
+            try:
+                temp_ply = get_suitable_version_path(path, temp_dir)
+                pc = o3d.io.read_point_cloud(temp_ply)
+            except Exception as e:
+                print(f"Erro ao tentar converter {path}: {e}")
+                return None
+        
+        print("==========================", len (np.array(pc.points)))
+
+        return pc
+
 def get_descriptor_function(name):
     mapping = {
-        #"spinimages": extract_spinimages,
+        # "spinimages": extract_spinimages,
         "rgb_covariance": compute_rgb_covariance_descriptor,
-        #"pfh": extract_pfh,
-        #"fpfh": extract_fpfh,
-        #"shot": extract_shot,
+        # "pfh": extract_pfh,
+        # "fpfh": extract_fpfh,
+        # "shot": extract_shot,
         # ...adicione outros aqui
     }
     if name in mapping:
         return mapping[name]
     raise ValueError(f'Descritor {name} não disponível.')
 
-"""
-# Função para calcular o histograma (se necessário)
-def compute_histogram(feature_vector, bins=64):
-    # Se o vetor já for um histograma, apenas retorne
-    if len(feature_vector.shape) == 1:
-        return feature_vector
-    # Caso contrário, calcule o histograma global
-    hist, _ = np.histogram(feature_vector, bins=bins, density=True)
-    return hist
-"""
 def feature_extraction(pc, descriptor):
     extract_func = get_descriptor_function(descriptor)
     feature_vector = extract_func(pc)
-    #feature_hist = compute_histogram(feature_vector)
+    # feature_hist = compute_histogram(feature_vector)
     return feature_vector
-
 
 def process_row(input_row, descriptor):
     ref_pc_path = os.path.join(str(input_row["REFLOCATION"]), str(input_row["REF"]))
@@ -73,6 +84,8 @@ def process_row(input_row, descriptor):
     test_pc = safe_read_point_cloud(test_pc_path)
     if ref_pc is None or test_pc is None:
         print(f"Erro ao processar linha: {input_row}")
+        print(f"ref={ref_pc}",ref_pc_path)
+        print(f"test={test_pc}",test_pc_path)
         return None
     ref_features = feature_extraction(ref_pc, descriptor)
     test_features = feature_extraction(test_pc, descriptor)
@@ -85,7 +98,7 @@ def process_row(input_row, descriptor):
         "ATTACK": input_row["ATTACK"],
         "CLASS": input_row["CLASS"]
     }
-    print(test_features) # teste
+    # Para cada feature extraída, adiciona no output_row
     for i, ref in enumerate(ref_features):
         output_row[f"fv_ref_{i}"] = ref
     for i, test in enumerate(test_features):
